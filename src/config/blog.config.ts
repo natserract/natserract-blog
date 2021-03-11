9daed7a284756566a7a76551805636d4e21b8259
natserract <benjaminstwo@gmail.com>
2021-03-11 22:09:55 +0800

    increase limit for posts per page

--- src/config/blog.config.ts
+++ src/config/blog.config.ts
@@ -8,7 +8,7 @@ export const blogConfig = {
     analytics: "", // put your google analytics code here to add tracking
     shareImage: "https://nextjs-mdx-blog-starter.now.sh/screenshot.png",
     shareImageAlt: "Next.js MDX Blog Starter Screenshot",
-    postsPerPage: 8,
+    postsPerPage: 100,
     showDate: true, // if you would like the date to appear on the blog posts
     disqus: "", // your disqus shortname, remove if you don't use disqus
 }
